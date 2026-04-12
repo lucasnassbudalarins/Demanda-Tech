e5d08ef2f38302f142419a79cc48b3f3a669264a
Lucas <lucasnassbudalarins@gmail.com>
2026-04-11 23:16:44 -0300

    first commit

---
+++ src/test/java/br/udesc/edu/demandatech/DemandaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.udesc.edu.demandatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandaTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
